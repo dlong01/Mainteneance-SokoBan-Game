5352720a32207a5619c11b1d8642fb07278c5ca5
Daniel Long <psydl2@exmail.nottingham.ac.uk>
2020-12-04 16:10:58 +0000

    JavaDoc COmments updated for Main Class, and javadocs generated for the first time

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 Git repository for Daniel Long's COMP2013 Coursework
+
+Initially worked on javadocs to help me understand what each Class was responsible for
--- src/sample/Main.java
+++ src/sample/Main.java
@@ -26,7 +26,6 @@ import java.io.FileNotFoundException;
 import java.io.InputStream;
 
 
-
 public class Main extends Application {
 
     private Stage primaryStage;
@@ -35,22 +34,30 @@ public class Main extends Application {
     private File saveFile;
     private MenuBar MENU;
 
-
+    /**
+     * The main method, calls launch from the application class.
+     * @param   args    Input on program start
+     */
     public static void main(String[] args) {
         launch(args);
         System.out.println("Done!");
     }
 
+    /**
+     * Defines the initial start conditions for the menus and the grid,
+     * overrides the start method of application.
+     * This method uses the {@link #loadDefaultSaveFile}
+     * and {@link StartMeUp}
+     * @param   primaryStage    The main game stage
+     */
     @Override
-    public void start(Stage primaryStage) throws Exception {
+    public void start(Stage primaryStage){
 /*
         Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
         primaryStage.setTitle("Hello World");
         primaryStage.setScene(new Scene(root, 300, 275));
         primaryStage.show();
 */
-
-
 
 
         this.primaryStage = primaryStage;
@@ -65,7 +72,8 @@ public class Main extends Application {
         MenuItem menuItemExit = new MenuItem("Exit");
         menuItemExit.setOnAction(actionEvent -> closeGame());
         Menu menuFile = new Menu("File");
-        menuFile.getItems().addAll(menuItemSaveGame, menuItemLoadGame, new SeparatorMenuItem(), menuItemExit);
+        menuFile.getItems().addAll(menuItemSaveGame, menuItemLoadGame,
+                new SeparatorMenuItem(), menuItemExit);
 
         MenuItem menuItemUndo = new MenuItem("Undo");
         menuItemUndo.setDisable(true);
@@ -95,7 +103,12 @@ public class Main extends Application {
         loadDefaultSaveFile(primaryStage);
     }
 
-    void loadDefaultSaveFile(Stage primaryStage) { this.primaryStage = primaryStage;
+    /**
+     * Loads the default SampleGame.skb and initializes the game.
+     * @param   primaryStage    The main game stage
+     */
+    void loadDefaultSaveFile(Stage primaryStage) {
+        this.primaryStage = primaryStage;
         System.out.println("Hi");
         InputStream in = getClass().getClassLoader().getResourceAsStream("sample/SampleGame.skb");
         System.out.println(in);
@@ -105,16 +118,33 @@ public class Main extends Application {
         System.out.println("Hi");
     }
 
+    /**
+     * Initializes a new gameEngine with {@link StartMeUp}
+     * using a .<!-- -->skb file as input.
+     * Once the engine is initialized the grid is reloaded.
+     * @param   input   The game file to initialize
+     */
     public void initializeGame(InputStream input) {
         gameEngine = new StartMeUp(input, true);
         reloadGrid();
     }
 
+    /**
+     * Creates an event to detect a key press.
+     * Upon key press {@link StartMeUp#handleKey} is called.
+     */
     public void setEventFilter() {
         primaryStage.addEventFilter(KeyEvent.KEY_PRESSED, event -> {
             gameEngine.handleKey(event.getCode());
             reloadGrid();
-        });}
+        });
+    }
+
+    /**
+     * Opens file search with filters for a .<!-- -->skb extension,
+     * loads chosen file using {@link #initializeGame}
+     * @throws FileNotFoundException    Throws an exception which is caught by the {@link #loadGame()}
+     */
     public void loadGameFile() throws FileNotFoundException {
         FileChooser fileChooser = new FileChooser();
         fileChooser.setTitle("Open Save File");
@@ -126,7 +156,14 @@ public class Main extends Application {
                 StartMeUp.logger.info("Loading save file: " + saveFile.getName());
             }
             initializeGame(new FileInputStream(saveFile));
-        }}private void reloadGrid() {
+        }
+    }
+
+    /**
+     * Checks if the game is completed, updates each of the positions on the grid.
+     * Uses {@link #showVictoryMessage()}
+     */
+    private void reloadGrid() {
         if (gameEngine.isGameComplete()) {
             showVictoryMessage();
             return;
@@ -137,10 +174,14 @@ public class Main extends Application {
         gameGrid.getChildren().clear();
         while (levelGridIterator.hasNext()) {
             addObjectToGrid(levelGridIterator.next(), levelGridIterator.getCurrentPosition());
-        }gameGrid.autosize();
+        }
+        gameGrid.autosize();
         primaryStage.sizeToScene();
     }
 
+    /**
+     * Displays a dialog box with the number of moves to complete the game.
+     */
     public void showVictoryMessage() {
         String dialogTitle = "Game Over!";
         String dialogMessage = "You completed " + gameEngine.getMapSetName() + " in " + gameEngine.getMovesCount() + " moves!";
@@ -149,6 +190,12 @@ public class Main extends Application {
         newDialog(dialogTitle, dialogMessage, mb);
     }
 
+    /**
+     * Creates a dialog box using the information from {@link  #showVictoryMessage()}
+     * @param   dialogTitle         The title for the dialog box
+     * @param   dialogMessage       The message in the dialog box
+     * @param   dialogMessageEffect The effect on the dialog box
+     */
     public void newDialog(String dialogTitle, String dialogMessage, Effect dialogMessageEffect) {
         final Stage dialog = new Stage();
         dialog.initModality(Modality.APPLICATION_MODAL);
@@ -174,16 +221,32 @@ public class Main extends Application {
         dialog.show();
     }
 
+    /**
+     * Creates a new {@link  #showVictoryMessage()} at a position, with game object as the parameter.
+     * @param   gameObject  The character at that position, defines whether it is a wall, air etc.
+     * @param   location    The position the game object needs to be added in.
+     */
     public void addObjectToGrid(GameObject gameObject, Point location) {
         GraphicObject graphicObject = new GraphicObject(gameObject);
         gameGrid.add(graphicObject, location.y, location.x);
     }
 
+    /**
+     * Ends the program when the exit menu option is chosen.
+     */
     public void closeGame() {
         System.exit(0);
     }
+
+    /**
+     * Saves the game when the save menu option is clicked.
+     */
     public void saveGame() {
     }
+
+    /**
+     * Runs the {@link  #loadGameFile()} method upon load menu option being selected.
+     */
     public void loadGame() {
         try {
             loadGameFile();
@@ -192,28 +255,43 @@ public class Main extends Application {
         }
     }
 
-    public void undo() { closeGame(); }
-    public void resetLevel() {}
+    /**
+     * Undoes the last move the player made
+     */
+    public void undo() {
+        closeGame();
+    }
 
+    /**
+     * Resets the level to the starting state
+     */
+    public void resetLevel() {
+    }
+
+    /**
+     *Displays a dialog box that contains information about the game.
+     */
     public void showAbout() {
         String title = "About This Game";
         String message = "Enjoy the Game!\n";
 
         newDialog(title, message, null);
     }
+
+    /**
+     * Allows the user to select whether of not they want music.
+     */
     public void toggleMusic() {
         // TODO
     }
+
+    /**
+     * Enables debug mode which makes it easier to understand the games processes.
+     */
     public void toggleDebug() {
         gameEngine.toggleDebug();
         reloadGrid();
     }
-
-
-
-
-
-
 
 
 }
