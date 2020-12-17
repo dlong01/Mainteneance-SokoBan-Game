df5ab959e4579641a4e82ddf925eaca8c2bd4b7b
Daniel Long <psydl2@examil.nottingham.ac.uk>
2020-12-17 13:59:54 +0000

    Wall and floor sprites created, .fxml for start screen created. Start Screen not implemented. No new bugs.

---
+++ src/startScreen/StartController.java
@@ -0,0 +1,24 @@
+package startScreen;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Slider;
+
+public class StartController {
+
+    @FXML
+    private Button startNew;
+
+    @FXML
+    private Slider wallSlider;
+
+    @FXML
+    private Slider floorSlider;
+
+    @FXML
+    private Button loadGame;
+
+    @FXML
+    private Button exitGame;
+
+}
